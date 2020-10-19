9bd53ebd2aa2731ff59953221e971e6099d74ce5
unknown <dev.samuellc@outlook.com>
2020-10-19 15:41:34 -0300

    commit

--- healthpage/src/pages/beauty.tsx
+++ healthpage/src/pages/beauty.tsx
@@ -20,7 +20,7 @@ function Beauty(){
                             <div className="dropdown-content">
                                     <Link to="/fitness" style={{ textDecoration: 'none' }}><p>Fitness</p></Link>
                                     <Link to="/nutrition" style={{ textDecoration: 'none' }}><p>Nutrição</p></Link>
-                                    <Link to=" beauty" style={{ textDecoration: 'none' }}><p>Ervas Medicinais</p></Link>
+                                    <Link to="/herbs" style={{ textDecoration: 'none' }}><p>Ervas Medicinais</p></Link>
                                     <Link to="/beauty" style={{ textDecoration: 'none' }}><p>Beleza e Estética</p></Link>
                                 </div>
                         </div>
@@ -39,9 +39,26 @@ function Beauty(){
            <div id="product-container">
                 <div id="product">
                     <h4>Bottox Caps</h4>
-                    <a href="http://makepages.com.br/r/AAT8741451"><img src="https://atardeservicosgraficos.com.br/wp-content/uploads/2020/07/bottox-caps.jpg" alt="" /></a>
+                    <a href="https://bottoxcaps.com.br/?mcr=AAT8741451"><img src="https://atardeservicosgraficos.com.br/wp-content/uploads/2020/07/bottox-caps.jpg" alt="" /></a>
                     <p className="prod-description">Cápsulas de colágeno hidrolisado que contribuem para a redução de linhas de expressão, rugas e pés de galinha. Recomendado por dermatologistas em 23 países e agora disponíveis no Brasil.</p>
                 </div>
+                <div id="product">
+                    <h4>Colágeno Larah Bárbara</h4>
+                    <a href="https://www.larahbarbara.com.br/?mcr=AQH9262324"><img src="https://cdn.awsli.com.br/600x1000/1591/1591645/produto/59285270/48b050d45d.jpg" alt="" /></a>
+                    <p className="prod-description">Cápsulas de colágeno hidrolisado que contribuem para a redução de linhas de expressão, rugas e pés de galinha. Recomendado por dermatologistas em 23 países e agora disponíveis no Brasil.</p>
+                </div>
+            </div>
+            <div id="product-container">
+                <div id="product">
+                    <h4>A S Sabonetes</h4>
+                    <a href="https://app.monetizze.com.br/r/AQQ9662911"><img src="https://scontent.fpfb1-1.fna.fbcdn.net/v/t1.0-9/118627825_212296466905473_4938072694210117419_o.jpg?_nc_cat=102&_nc_sid=8bfeb9&_nc_ohc=tSGdetm6xl0AX-qMF0g&_nc_ht=scontent.fpfb1-1.fna&oh=9f09f18c579c04e32380561e9e251194&oe=5FB3C0FE" alt="" /></a>
+                    <p className="prod-description">Este sabonete natural feito de ervas é destinado para pessoas com problemas de pele, tais como dermatite, frieira, psoríase, espinhas e etc.</p>
+                </div>
+                {/*<div id="product">
+                    <h4>Colágeno Larah Bárbara</h4>
+                    <a href="https://www.larahbarbara.com.br/?mcr=AQH9262324"><img src="https://cdn.awsli.com.br/600x1000/1591/1591645/produto/59285270/48b050d45d.jpg" alt="" /></a>
+                    <p className="prod-description">Cápsulas de colágeno hidrolisado que contribuem para a redução de linhas de expressão, rugas e pés de galinha. Recomendado por dermatologistas em 23 países e agora disponíveis no Brasil.</p>
+        	    </div>*/}
             </div>
             
         </div>
--- healthpage/src/pages/fitness.tsx
+++ healthpage/src/pages/fitness.tsx
@@ -39,24 +39,24 @@ function Fitness(){
            <div id="product-container">
                 <div id="product">
                     <h4>Xtreme Fit</h4>
-                    <a href="http://makepages.com.br/r/AKS9260909?u=c&pl=NZ68690"><img src="https://corpinholindo.com/wp-content/uploads/2020/06/XTREME-FIT-Funciona-Bula-Composi%C3%A7%C3%A3o-F%C3%B3rmula-Ingredientes-pre%C3%A7o-Comprar.png" alt="" /></a>
+                    <a href="https://app.monetizze.com.br/checkout/DNZ68690"><img src="https://corpinholindo.com/wp-content/uploads/2020/06/XTREME-FIT-Funciona-Bula-Composi%C3%A7%C3%A3o-F%C3%B3rmula-Ingredientes-pre%C3%A7o-Comprar.png" alt="" /></a>
                     <p className="prod-description">O Xtreme Fit é um produto eficaz, seguro e 100% natural que consegue te ajudar a emagrecer de uma vez por todas. O Xtreme fit garante um emagrecimento totalmente saudável, sem receitas médicas e sem riscos à saúde, oferecendo um emagrecimento definitivo.</p>
                 </div>
                 <div id="product">
                     <h4>Detox Caps</h4>
-                    <a href="http://makepages.com.br/r/ADE8730931"><img src="https://detoxcaps.com.br/wp-content/uploads/2020/03/030303.png" alt="" /></a>
+                    <a href="https://detoxcaps.com.br/?mcr=ADE8730931"><img src="https://detoxcaps.com.br/wp-content/uploads/2020/03/030303.png" alt="" /></a>
                     <p className="prod-description">O Detox Caps ajuda no emagrecimento reduzindo o apetite, aumentando a disposição e eliminando a retenção líquida do corpo, proporcionando resultados visíveis e duradouros.</p>
                 </div>
             </div>
             <div id="product-container">
                 <div id="product">
                     <h4>Sinedrol</h4>
-                    <a href="http://makepages.com.br/r/ABQ11196298"><img src="https://arscientia.com.br/wp-content/uploads/2020/01/sinedrol.jpg" alt="" /></a>
+                    <a href="https://sinedrol.com.br/?mcr=ABQ11196298"><img src="https://arscientia.com.br/wp-content/uploads/2020/01/sinedrol.jpg" alt="" /></a>
                     <p className="prod-description">O Sinedro é um suplemento encapsulado de alta tecnologia e performance. Uma fórmula exclusiva com grandes benefícios no ganho de massa magra, emagrecimento, força e definição além de outros benefícios a saúde.</p>
                 </div>
                 <div id="product">
                     <h4>Sibutina</h4>
-                    <a href="https://www.makepages.com.br/r/AXC11198354"><img src="https://s3-sa-east-1.amazonaws.com/monetizzeappbr/img/fotos/121947/1dcb528cb16d7bafc94a3927e68b58e6.png" alt="" /></a>
+                    <a href="https://sibutina.com.br/?mcr=AXC11198354"><img src="https://s3-sa-east-1.amazonaws.com/monetizzeappbr/img/fotos/121947/1dcb528cb16d7bafc94a3927e68b58e6.png" alt="" /></a>
                     <p className="prod-description">A Sibutina é uma fórmula que auxilia no rápido emagrecimento e eliminação de gordura profunda.</p>
                 </div>
             </div>
